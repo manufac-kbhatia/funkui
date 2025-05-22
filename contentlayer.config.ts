62ebab8ce5f8de4317d1f55e9a422f99ba985627
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-05-22 11:13:21 +0530

    Added heading as computed field for adding TOC

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,6 +1,8 @@
 import { defineDocumentType, makeSource } from 'contentlayer2/source-files'
 import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
+import GithubSlugger from "github-slugger"
+
 
 export const Doc = defineDocumentType(() => ({
   name: 'Doc',
@@ -21,6 +23,27 @@ export const Doc = defineDocumentType(() => ({
         resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
       },
       url: {type: "string", resolve: (doc) => `/${doc._raw.flattenedPath}`},
+      // Ref:  https://yusuf.fyi/posts/contentlayer-table-of-contents/
+      headings: {
+        type: "json",
+        resolve: async (doc) => {
+          const regXHeader = /\n(?<flag>#{1,6})\s+(?<content>.+)/g;
+          const slugger = new GithubSlugger()
+          const headings = Array.from(doc.body.raw.matchAll(regXHeader)).map(
+              ({ groups }) => {
+                const flag = groups?.flag;
+                const content = groups?.content;
+                return {
+                  level: flag?.length,
+                  text: content,
+                  slug: content ? slugger.slug(content) : undefined
+                };
+              }
+            );
+            return headings;
+          },
+      }
+      
   },
 }))
 
